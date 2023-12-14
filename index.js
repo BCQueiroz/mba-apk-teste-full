301d63339d52e090da9859c354abdcb147829f9c
bcqueiroz <bcqueiroz123@gmail.com>
2023-12-13 22:07:02 -0300

    feat: nova feature

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
 
+app.get('/', (req, res) => {
+    console.log('Nova feature.')
+})
+
 app.listen(port, () => {
     console.log(`Testando servidor, ${port}`)
 })
