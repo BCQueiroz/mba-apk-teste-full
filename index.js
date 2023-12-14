c7e15379beea24712aed5e2074771d935ada94ec
bcqueiroz <bcqueiroz123@gmail.com>
2023-12-13 22:56:30 -0300

    feat: testando api cool-faces

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const port = process.env.PORT || 3001
 const app = express()
 
 app.get('/', (req, res) => {
-    console.log('Nova feature.')
+    console.log('Hello World.')
+})
+
+app.get('/cool', (req, res) => {
+    res.send(cool())
+})
+
+app.get('/happy', (req, res) => {
+    console.log('Nova feature3.')
 })
 
 app.listen(port, () => {
